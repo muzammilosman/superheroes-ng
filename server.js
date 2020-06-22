bd59364e0487c3a27a999b71468a85463588e646
muzammilosman <usman.muzammil@gmail.com>
2020-06-22 21:29:59 +0530

    Cors middleware in server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/superheroes-ng'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/superheroes-ng/index.html'));
